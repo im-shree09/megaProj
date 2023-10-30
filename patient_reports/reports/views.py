a203e03aa3d231bfcd37bd14871891b91687c163
Mrunal <mrunalkhade35@gmail.com>
2023-10-30 14:15:29 +0530

    almost done

--- patient_reports/reports/views.py
+++ patient_reports/reports/views.py
@@ -9,12 +9,19 @@ from django.contrib.auth.decorators import login_required
 
 
 def home(request):
-    return render(request, 'reports/index.html')
+    return render(request, 'reports/land.html')
+
+def dash(request):
+    return render(request, 'dashboard.html')
+
+def docdash(request):
+    return render(request, 'docdash.html')
+
 
 def report_list(request):
     reports = PatientReport.objects.all()
-    print(reports)
     return render(request, 'reports/report_list.html', {'reports': reports})
+
 
 def upload_report(request):
     if request.method == 'POST':
@@ -28,7 +35,7 @@ def upload_report(request):
 
 def view_report(request, pk):
     report = PatientReport.objects.get(pk=pk)
-    return render(request, 'view_report.html', {'report': report})
+    return render(request, 'details.html', {'report': report})
 
 
 def lp(request, pk):
@@ -76,6 +83,37 @@ def register(request):
             my_user.save()
             return redirect('loginn')
     return render (request,'singup.html')
+
+def doctor_login(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        user=authenticate(request,username=username,password=pass1)
+        if user is not None:
+            login(request,user)
+            return redirect('docdash')
+        else:
+            return HttpResponse ("Username or Password is incorrect!!!")
+
+    return render (request,'doctor_login.html')
+
+def doctor_register(request):
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+
+        if pass1!=pass2:
+            return HttpResponse("Your password and confrom password are not Same!!")
+        else:
+
+            my_user=User.objects.create_user(uname,email,pass1)
+            my_user.save()
+            return redirect('doctor_login')
+    return render (request,'doctor_register.html')
+
+
 
 def LogoutPage(request):
     logout(request)
